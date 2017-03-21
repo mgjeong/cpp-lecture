179b8e96a32c25330b0b887e5760c9014ee004c5
myeong.jeong <myeong.jeong@samsung.com>
2017-03-21 21:11:20 +0900

    Add overloading01 file.
    Signed-off-by: myeong.jeong <myeong.jeong@samsung.com>

---
+++ lecture01/Overloading01.cpp
@@ -0,0 +1,29 @@
+/**
+ *  @file       Overloading01.cpp
+ *  @author     mg.jeong<myeong.jeong@samsung.com>
+ */
+
+#include <iostream>
+using namespace std;
+
+int square ( int a )
+{
+    return a * a;
+}
+
+double square ( double d )
+{
+    return d * d;
+}
+
+int main( int argc, char* argv[] )
+{
+    int n = square ( 3 );
+    double d = square ( 3.5 );
+
+    cout << "square( 3 ) = " << n << endl;
+    cout << "square( 3.5 ) = " << d << endl;
+
+    return 0;
+}
+
