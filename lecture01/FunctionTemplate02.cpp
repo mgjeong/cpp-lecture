390b7f423952eec88da6ab716c3db7af152fa522
myeong.jeong <myeong.jeong@samsung.com>
2017-04-10 19:51:36 +0900

    Add FunctionTemplate02 example
    Signed-off-by: myeong.jeong <myeong.jeong@samsung.com>

---
+++ lecture01/FunctionTemplate02.cpp
@@ -0,0 +1,25 @@
+/**
+ *  @file       FunctionTemplate02.cpp
+ *  @author     mg.jeong<myeong.jeong@samsung.com>
+ */
+
+#include <iostream>
+using namespace std;
+
+#define MAKE_MAX( T )   \
+   T    Max( T a, T b ) { return a > b ? a : b; }
+
+MAKE_MAX( int )
+MAKE_MAX( double )
+
+int main( int argc, char* argv[] )
+{
+    int n = Max( 1, 2 );
+    double d = Max( 3.1, 4.5 );
+
+    cout << "Max( 1, 2 ) = " << n << endl;
+    cout << "Max( 3.1, 4.5 ) = " << d << endl;
+
+    return 0;
+}
+
