915406bf55b8630d1ef4f94f5a613073ac8bdf27
myeong.jeong <myeong.jeong@samsung.com>
2017-04-11 07:12:05 +0900

    Add Inline01 example file.
    Signed-off-by: myeong.jeong <myeong.jeong@samsung.com>

---
+++ lecture01/Inline01.cpp
@@ -0,0 +1,28 @@
+/**
+ *  @file       Inline01.cpp
+ *  @author     mg.jeong <myeong.jeong@samsung.com>
+ */
+
+#include <iostream>
+using namespace std;
+
+int Add1( int a, int b )
+{
+    return a + b;
+}
+
+inline
+int Add2( int a, int b )
+{
+    return a + b;
+}
+
+
+int main( int argc, char* argv[] )
+{
+    int n1 = Add1( 1, 2 );
+    int n2 = Add2( 1, 2 );
+
+    return 0;
+}
+
