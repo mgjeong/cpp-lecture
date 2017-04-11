e3cffb6d9a4c860289f893aa9cfb0dc35d083840
myeong.jeong <myeong.jeong@samsung.com>
2017-04-12 08:16:25 +0900

    Add MemberFunctionPointer02 example
    Signed-off-by: myeong.jeong <myeong.jeong@samsung.com>

---
+++ lecture01/MemberFunctionPointer02.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @file       MemberFunctionPointer02.cpp
+ *  @author     mg.jeong <myeong.jeong@samsung.com>
+ */
+
+#include <iostream>
+using namespace std;
+
+class Dialog
+{
+public :
+    void Close( )
+    {
+        cout << "Close dialog" << endl;
+    }
+    static void Close2( )
+    {
+        cout << "Close dialog 2" << endl;
+    }
+};
+
+void foo( )
+{
+    cout << "foo" << endl;
+}
+
+int main( int argc, char* argv[] )
+{
+    void (*f1)( void ) = &foo;
+    void (*f2)( ) = &Dialog::Close;  // ???? 
+    void (*f3)( void ) = &Dialog::Close2; // ??
+
+    f1();
+    f3();
+
+    return 0;
+}
+
